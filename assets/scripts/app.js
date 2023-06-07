222943261ef5a7a07d667b6894c51647ccb809b4
ovovvvvv <ovovvvvv@naver.com>
2023-06-07 21:25:33 +0900

    객체 생성하기

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,8 +18,14 @@ function add() {
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
-  logEntries.push(enteredNumber);
-  console.log(logEntries[0]);
+  const logEntry = {
+    operation: "ADD",
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 function subtract() {
