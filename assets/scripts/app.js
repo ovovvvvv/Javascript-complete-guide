2b9b114a817b3f34be1787edb209eb25aa0682c7
ovovvvvv <ovovvvvv@naver.com>
2023-06-26 14:41:34 +0900

    &와 || 을 사용해 조건문 결합

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,22 +30,38 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  let mathOperator;
+  // if (
+  //   calculationType !== "ADD" &&
+  //   calculationType !== "SUBTRACT" &&
+  //   calculationType !== "MULTIPLY" &&
+  //   calculationType !== "DIVIDE"
+  // ) {
+  //   return;
+  // }
 
-  if (calculationType === "ADD") {
-    currentResult += enteredNumber;
-    mathOperator = "+";
-  } else if (calculationType === "SUBTRACT") {
-    currentResult -= enteredNumber;
-    mathOperator = "-";
-  } else if (calculationType === "MULTIPLY") {
-    currentResult *= enteredNumber;
-    mathOperator = "*";
-  } else {
-    currentResult /= enteredNumber;
-    mathOperator = "/";
+  if (
+    calculationType === "ADD" ||
+    calculationType === "SUBTRACT" ||
+    calculationType === "MULTIPLY" ||
+    calculationType === "DIVIDE"
+  ) {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    let mathOperator;
+
+    if (calculationType === "ADD") {
+      currentResult += enteredNumber;
+      mathOperator = "+";
+    } else if (calculationType === "SUBTRACT") {
+      currentResult -= enteredNumber;
+      mathOperator = "-";
+    } else if (calculationType === "MULTIPLY") {
+      currentResult *= enteredNumber;
+      mathOperator = "*";
+    } else {
+      currentResult /= enteredNumber;
+      mathOperator = "/";
+    }
   }
 
   createAndWriteOutput(mathOperator, initialResult, enteredNumber);
